1e6a42ffb761c6522ff9837c681e2c1a0b7dbdbc
mattcollins77 <matthew_collins@icloud.com>
2024-01-15 16:29:49 +0000

    sssd

--- entry.sh
+++ entry.sh
@@ -3,6 +3,7 @@
 # Source the setup script to set up the ROS environment
 source /opt/ros/${ROS_DISTRO}/setup.bash
 source /home/astrodeck_ws/install/setup.bash
+source install/setup.bash
 
 # Run colcon to build your ROS workspace (optional, if not already built)
 colcon build --symlink-install
