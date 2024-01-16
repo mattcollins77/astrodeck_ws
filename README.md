ea7e7a86440926683cdac64e8e63e2135306bf81
mattcollins77 <matthew_collins@icloud.com>
2024-01-16 14:10:19 +0000

    asdsad

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-Matts notes:
-
-On PI or Steamdeck in workspace DIR
-git pull
-
-// BUILD IMAGE
-docker build -f pi5.Dockerfile -t piimage .
-OR
-docker build -f steamdeck.Dockerfile -t piimage .
-
-//this runs a container and mounts the volume
-docker run -it --name PiContainer --network host -v $(pwd)/src:/home/astrodeck_ws/src  piimage
+Matt
 
 
--- entry.sh
+++ entry.sh
@@ -2,12 +2,9 @@
 
 # Source the setup script to set up the ROS environment
 source /opt/ros/${ROS_DISTRO}/setup.bash
-source /home/astrodeck_ws/install/setup.bash
-source install/setup.bash
 
 # Run colcon to build your ROS workspace (optional, if not already built)
 colcon build --symlink-install
-source /home/astrodeck_ws/install/setup.bash
 source install/setup.bash
 # Execute the ROS launch file
 /bin/bash
