6ddb9d558eb4991aa4b6eba83757b57cd99777a9
mattcollins77 <matthew_collins@icloud.com>
2024-01-22 09:36:58 +0000

    dd

--- testscript.py
+++ testscript.py
@@ -1,8 +1,11 @@
 import maestro
 import time
+import serial
 
 # Define the serial port (update with your correct port)
 serial_port = '/dev/ttyACM1'  # Use 'ls /dev/tty*' to find the correct port
+teensy_port = '/dev/ttyACM0'
+baudrate = 9600
 
 # Open a connection to the Maestro controller
 servo = maestro.Controller(serial_port)
@@ -16,3 +19,11 @@ servo.runScriptSub(subroutine_number)
 
 # Close the serial connection when done
 servo.close()
+# Create a serial connection
+with serial.Serial(teensy_port, baudrate, timeout=1) as ser:
+    while True:
+        ser.write(b'<SH1>')  # Send the command
+        print("Command sent: <SH1>")
+        
+        # Wait for 10 seconds
+        time.sleep(10)
